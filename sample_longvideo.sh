174e77af03181c6a9da93cda0421ab35ea64a8fb
JianxiongGao <50777888+JianxGao@users.noreply.github.com>
2025-12-25 02:31:07 +0800

    Update sample_longvideo.sh

--- sample_longvideo.sh
+++ sample_longvideo.sh
@@ -1,4 +1,3 @@
-export http_proxy=http://10.66.70.227:11080 https_proxy=http://10.66.70.227:11080 no_proxy=localhost,127.0.0.1,localaddress,localdomain.com,internal,corp.kuaishou.com,test.gifshow.com,staging.kuaishou.com
 python inference.py \
     --json_file  ./example/ride_horse/cond.json \
     --image_path ./example/ride_horse/first.png  \
