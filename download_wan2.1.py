5d1495cd7d23044bfc2be1c6cf34e887db4a3dde
JianxiongGao <50777888+JianxGao@users.noreply.github.com>
2026-01-07 17:08:28 +0800

    Update download_wan2.1.py

--- download_wan2.1.py
+++ download_wan2.1.py
@@ -3,3 +3,4 @@ from modelscope import snapshot_download
 
 # Download models
 snapshot_download("Wan-AI/Wan2.1-I2V-14B-480P", local_dir="models/Wan-AI/Wan2.1-I2V-14B-480P")
+snapshot_download("Wan-AI/Wan2.1-T2V-1.3B", local_dir="models/Wan-AI/Wan2.1-T2V-1.3B")
